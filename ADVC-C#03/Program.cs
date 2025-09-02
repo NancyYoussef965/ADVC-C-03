ceb74a60e4de7150ecf5d211087fe21d1e58f293
Nancy <ny6049812@gmail.com>
2025-09-02 20:20:06 +0300

    Q1

--- ADVC-C#03/Program.cs
+++ ADVC-C#03/Program.cs
@@ -6,7 +6,36 @@ namespace MyApp
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+
+            #region 1-Given an array of integers, count the frequency of each element using a hash table.
+
+            int[] numbers = { 1, 2, 2, 3, 4, 1, 5, 2, 3, 3 };
+
+            Dictionary<int, int> frequency = new Dictionary<int, int>();
+            foreach (int num in numbers)
+            {
+                if(frequency.ContainsKey(num))
+                    frequency[num]++;  
+        
+            else
+            {
+                frequency[num] = 1; 
+            }
+
+        }
+            Console.WriteLine(" Frequency of elements:");
+            foreach (var kvp in frequency)
+            {
+                Console.WriteLine($"Element {kvp.Key} appears {kvp.Value} times");
+            }
+            #endregion
+
+
+
+
+
+
+
         }
     }
 }
