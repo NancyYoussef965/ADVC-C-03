280c402f2df57fc8f7205c7592ae5e69b7acafe4
Nancy <ny6049812@gmail.com>
2025-09-02 20:27:42 +0300

    q2

--- ADVC-C#03/Program.cs
+++ ADVC-C#03/Program.cs
@@ -9,28 +9,48 @@ namespace MyApp
 
             #region 1-Given an array of integers, count the frequency of each element using a hash table.
 
-            int[] numbers = { 1, 2, 2, 3, 4, 1, 5, 2, 3, 3 };
+            //    int[] numbers = { 1, 2, 2, 3, 4, 1, 5, 2, 3, 3 };
 
-            Dictionary<int, int> frequency = new Dictionary<int, int>();
-            foreach (int num in numbers)
-            {
-                if(frequency.ContainsKey(num))
-                    frequency[num]++;  
-        
-            else
-            {
-                frequency[num] = 1; 
-            }
+            //    Dictionary<int, int> frequency = new Dictionary<int, int>();
+            //    foreach (int num in numbers)
+            //    {
+            //        if(frequency.ContainsKey(num))
+            //            frequency[num]++;  
 
-        }
-            Console.WriteLine(" Frequency of elements:");
-            foreach (var kvp in frequency)
-            {
-                Console.WriteLine($"Element {kvp.Key} appears {kvp.Value} times");
-            }
+            //    else
+            //    {
+            //        frequency[num] = 1; 
+            //    }
+
+            //}
+            //    Console.WriteLine(" Frequency of elements:");
+            //    foreach (var kvp in frequency)
+            //    {
+            //        Console.WriteLine($"Element {kvp.Key} appears {kvp.Value} times");
+            //    }
             #endregion
 
 
+            #region 2-You have a hashtable where its values are integers, find the key associated with the highest value.
+
+            Dictionary<string, int> scores = new Dictionary<string, int>()
+            {
+
+            { "Ali", 85 },
+            { "Mona", 92 },
+            { "Sara", 78 },
+            { "Omar", 95 },
+            { "Nader", 88 }
+            };
+
+            var maxEntry = scores.Aggregate((l, r) => l.Value > r.Value ? l : r);
+
+            Console.WriteLine($" The highest score is {maxEntry.Value}, by {maxEntry.Key}");
+
+
+
+
+            #endregion
 
 
 
